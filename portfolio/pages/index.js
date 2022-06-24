466e3962a54a59ae404fb71e75c8c54bfbe5c5bb
SankalpSTG <sankalppolk123@gmail.com>
2022-06-25 02:55:46 +0530

    Created Next App

---
+++ portfolio/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+class Home extends React.Component{
+  render(){
+    return <>My Page</>
+  }
+}
+
+export default Home
