46fee6c221f2066c761b82cd2edc7d20e0571ace
sankalppolk123 <sankalppolk123@gmail.com>
2023-03-03 19:44:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
             <div className="tw-w-full tw-flex tw-items-center tw-justify-center tw-h-[100vh]">
               <div className="tw-w-full tw-max-w-[480px] tw-text-left tw-px-10">
                 <p className="tw-text-[36px] tw-leading-none">I am</p>
-                <p className="tw-text-[48px]">Sankalp Pol</p>
+                <p className="tw-text-[48px] tw-leading-lg">Sankalp Pol</p>
+                <p className="tw-text-[20px] tw-leading-none">(Full Stack Developer)</p>
                 <div className="tw-bg-[#1F2937] tw-w-full tw-max-w-[480px] tw-my-2 tw-p-2 tw-rounded"><FontAwesomeIcon icon={faPhone}/> <span className="tw-pl-4">+91 996-726-8418</span></div>
                 <div className="tw-bg-[#1F2937] tw-w-full tw-max-w-[480px] tw-my-4 tw-p-2 tw-rounded"><FontAwesomeIcon icon={faEnvelope}/> <span className="tw-pl-4">sankalppolk123@gmail.com</span></div>
                 <div className="tw-flex tw-gap-2 tw-mt-3">
@@ -21,10 +22,16 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="tw-flex tw-items-center tw-justify-center tw-p-5"><img src={SankalpPol1Image} className="tw-rounded-full tw-w-full tw-max-w-[360px] tw-relative" style={{boxShadow: "0px 0px 3px #000000"}} alt="Sankalp Pol"/></div>
+            <div className="tw-flex tw-items-center tw-justify-center tw-p-5 tw-h-[100vh]"><img src={SankalpPol1Image} className="tw-rounded-full tw-w-full tw-max-w-[360px] tw-relative" style={{boxShadow: "0px 0px 3px #000000"}} alt="Sankalp Pol"/></div>
           </div>
         </div>
-        <div className="tw-hidden">
+        <div className="tw-min-h-[100vh] tw-bg-[#171717] tw-p-5 tw-flex tw-items-center tw-justify-center">
+          <div className="tw-text-center tw-text-white">
+            <div className="tw-text-[24px]"><b>ABOUT ME</b></div>
+            <div className="tw-text-[20px] tw-w-full tw-max-w-[576px]">Software Developer with about 2 years of experience in MERN Stack, Golang and Android Development(Kotlin).</div>
+          </div>
+        </div>
+        <div className="tw-hidden tw-min-h-[100vh] tw-bg-[#18181B]">
           <div>Education</div>
           <div>
             <div>Bachelors in Computer Science, MIT Aurangabad</div>
