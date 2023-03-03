796c9ae87bcd5a7df37fae2fb9fd0f30024c79f8
sankalppolk123 <sankalppolk123@gmail.com>
2023-03-03 14:41:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBDLiF7s4y7RATxOQLFN8mxIg4n0mxtGi0",
-  authDomain: "sankalp-pol.firebaseapp.com",
-  projectId: "sankalp-pol",
-  storageBucket: "sankalp-pol.appspot.com",
-  messagingSenderId: "772789006597",
-  appId: "1:772789006597:web:f32502842b11db93a5876c"
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
 
 function App() {
   return (
